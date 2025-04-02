aced2840a9289b97888b64cf39d6345a904bdb4b
Pedro Chame <pedrohik@gmail.com>
2025-04-02 10:33:08 -0300

    Correção de caminho para banco de dados.

--- backend/DAO/questionDAO.py
+++ backend/DAO/questionDAO.py
@@ -1,12 +1,16 @@
 import sqlite3
 from model import Question
 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Obtém o caminho do script
+DB_PATH = os.path.join(BASE_DIR, "../banco.db")  # Caminho absoluto
+
 class QuestionDAO:
     def __init__(self):
         pass
 
     def getQuestions(self):
-        conexao = sqlite3.connect("backend/banco.db")
+        conexao = sqlite3.connect(DB_PATH)
         registros = conexao.cursor().execute("select * from questions").fetchall()
         questions = []
         for r in registros: 
--- backend/DAO/rankingDAO.py
+++ backend/DAO/rankingDAO.py
@@ -1,12 +1,16 @@
 import sqlite3
 from model import Ranking
 
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Obtém o caminho do script
+DB_PATH = os.path.join(BASE_DIR, "../banco.db")  # Caminho absoluto
+
 class RankingDAO:
     def __init__(self):
         pass
 
     def getRanking(self):
-        conexao = sqlite3.connect("backend/banco.db")
+        conexao = sqlite3.connect(DB_PATH)
         registros = conexao.cursor().execute("select * from ranking order by score desc").fetchall()
         rankings = []
         for r in registros:
@@ -17,7 +21,7 @@ class RankingDAO:
     
     def createRanking(self, name, score):
         try:
-            conexao = sqlite3.connect("backend/banco.db")
+            conexao = sqlite3.connect(DB_PATH)
             conexao.cursor().execute("insert into ranking(name,score) values(?,?)",(name,score))       
             conexao.commit()
             conexao.close()
